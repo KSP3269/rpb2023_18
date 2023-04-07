a3a74fe628ba6a953973b83dd55ad330ce561ec5
KSP3269 <130023377+KSP3269@users.noreply.github.com>
2023-04-07 11:11:04 +0900

    Update calc.py

--- git/calc.py
+++ git/calc.py
@@ -5,10 +5,10 @@ import add
 x = int(input())
 y = int(input())
 
-print(x,"+",y,"=",add(x,y))
-print(x,"-",y,"=",add(x,-y))
-print(x,"x",y,"=",multiply(x,y))
+print(x,"+",y,"=",add.add(x,y))
+print(x,"-",y,"=",add.add(x,-y))
+print(x,"x",y,"=",multiply.multiply(x,y))
 if y!=0 :
-  print(x,"/",y,"=",divide(x,y))
+  print(x,"/",y,"=",divide.divide(x,y))
 else:
   print("you cannot divide by 0")
