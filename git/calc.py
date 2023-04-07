9b2710145bc308798ee6c78b49ce8825c82ee858
KSP3269 <130023377+KSP3269@users.noreply.github.com>
2023-04-07 11:02:47 +0900

    Add files via upload

---
+++ git/calc.py
@@ -0,0 +1,8 @@
+import multiply
+import divide
+import add
+
+x = int(input())
+y = int(input())
+
+print(%d +"+"+ %d + "="+ %d (x,y,add(x,y)))
---
+++ git/divide.py
@@ -0,0 +1,5 @@
+def divide(a,b):
+	if (b!=0):
+		return a/b
+	else:
+		return 0
